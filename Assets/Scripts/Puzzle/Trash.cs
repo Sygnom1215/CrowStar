c7d68ea4cc471b2634b0422b7bbca9613485c5d7
minyoung529 <77655318+minyoung529@users.noreply.github.com>
2021-12-23 20:28:43 +0900

    첫번쨰 쓰레기 끝남

--- Assets/Scripts/Puzzle/Trash.cs
+++ Assets/Scripts/Puzzle/Trash.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Trash : MonoBehaviour
 {
+    Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
     private void OnMouseDrag()
     {
         transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -12,6 +19,6 @@ public class Trash : MonoBehaviour
 
     private void OnMouseUp()
     {
-        
+        rigid.AddForce(transform.position * 0.5f, ForceMode2D.Impulse);
     }
 }
--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject trashObject;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartTrashPuzzle();
+    }
+    public void StartTrashPuzzle()
+    {
+        for(int i = 0; i<35 ; i++)
+        {
+            GameObject obj = Instantiate(trashObject, trashObject.transform.parent);
+            obj.transform.position = new Vector3(Random.Range(-9f, 9f), Random.Range(-4f, 4f), 10f);
+            obj.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, Random.Range(0f, 360f)));
+        }
     }
 }
