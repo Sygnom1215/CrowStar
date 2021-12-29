ff01608e601fbaf9733cdeeea642d1c6c6ef0cec
minyoung529 <77655318+minyoung529@users.noreply.github.com>
2021-12-29 18:18:05 +0900

    메인화면 UI만 만들었습니다

---
+++ Assets/Scripts/MainSceneButton.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class MainSceneButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Text text;
+    [SerializeField] Color color;
+    Color originColor;
+
+    private void Start()
+    {
+        text = GetComponentInChildren<Text>();
+        originColor = text.color;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = color;
+        transform.DOScale(1.2f, 0.3f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = originColor;
+        transform.DOScale(1f, 0.3f);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene(ConstantManager.MAIN_SCENE);
+    }
+}
