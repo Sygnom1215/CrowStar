94c1571117d102eb6f4b6fe2fa7bedbb2ba78aa1
zanke27 <zanke27@gmail.com>
2022-01-03 14:08:36 +0900

    이제 대화 할 거에 따라서 ChooseConversation(할 대화)로 쓸 수 있음 근데 이미 병합해놨네 걍 저기 가서 다시 만들어야지

---
+++ Assets/Scripts/Talk/OldDialogDisplay.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class OldDialogDisplay : MonoBehaviour
+{
+    public Conversation conversation = null;
+
+    public GameObject talkerLeft;
+    public GameObject talkerRight;
+
+    public GameObject talkerObserver;
+
+    public TalkUI talkerUILeft;
+    public TalkUI talkerUIRight;
+
+    public TalkUI talkerUIObserver;
+
+    //public GameObject endSceneImage;
+
+    private int activeLineIndex = 0;
+
+    private void Start()
+    {
+        talkerUILeft = talkerLeft.GetComponent<TalkUI>();
+        talkerUIRight = talkerRight.GetComponent<TalkUI>();
+
+        talkerUIObserver = talkerObserver.GetComponent<TalkUI>();
+
+        //talkerUILeft.Talker = conversation.talkerLeft;
+        //talkerUIRight.Talker = conversation.talkerRight;
+
+        //talkerUIObserver.Talker = conversation.talkerObserver;
+
+        AdvanceConversation();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            AdvanceConversation();
+        }
+    }
+    void AdvanceConversation()
+    {
+        if (activeLineIndex < conversation.lines.Length)
+        {
+            DisplayLine();
+            activeLineIndex += 1;
+        }
+        else
+        {
+            talkerUILeft.Hide();
+            talkerUIRight.Hide();
+            talkerUIObserver.Hide();
+            activeLineIndex = 0;
+            //endSceneImage.transform.DOMove(new Vector3(0, 0), 1);
+        }
+    }
+    void DisplayLine()
+    {
+        Line line = conversation.lines[activeLineIndex];
+        Character character = line.character;
+
+        if (talkerUILeft.TalkerIs(character))
+        {
+            SetDialog(talkerUILeft, talkerUIRight, line.text);
+        }
+        else if(talkerUIRight.TalkerIs(character))
+        {
+            SetDialog(talkerUIRight, talkerUILeft, line.text);
+        }
+        else
+        {
+            SetDialog(talkerUIObserver, talkerUIRight, line.text);
+        }
+    }
+    void SetDialog(TalkUI activeTalkerUI, TalkUI inactiveTalkerUI, string text)
+    {
+        inactiveTalkerUI.Hide();
+        activeTalkerUI.Dialog = text;
+        activeTalkerUI.Show();
+    }
+}
